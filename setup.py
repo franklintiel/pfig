5f2e8851b1517e34c8f8d58d4bc770192cb2784c
Franklin Sarmiento <franklinitiel@gmail.com>
2019-01-04 13:01:27 -0500

    Stable version defined.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="pfpi",
-    version="1.0.0",
+    version="1.0.1",
     author="Franklin Sarmiento",
     author_email="franklinitiel@gmail.com",
     description="Interface to do payments through Paguelo Facil gateway",
